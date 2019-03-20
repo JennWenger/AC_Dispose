77b08936807686fe30bb37e9e0c4a26afe955754
Jennifer Wenger <jennifer@techiewebdesigns.com>
2019-03-20 10:29:38 -0400

    Add files via upload

---
+++ Dispose/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var watcher = new FileSystemWatcher(@"C:\temp") { EnableRaisingEvents = true })
+            {
+                watcher.Created += new FileSystemEventHandler(watcher_Created);
+
+                Console.WriteLine("Press a key to release the watcher reference.");
+                Console.ReadKey(true);
+
+                watcher = null;
+            }
+            Watcher.Dispose();
+            Console.WriteLine("Press a key to exit the application.");
+            Console.ReadKey(true);
+        }
+
+        static void watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File created");
+            Console.WriteLine("Completed");
+        }
+    }
+}
